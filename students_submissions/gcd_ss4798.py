de3ffa170ac10fd91af6e0c1992d72896718cdcb
Sunny Soma <ss4798@njit.edu>
2026-02-08 16:32:05 -0500

    Implemented GCD Function

---
+++ students_submissions/gcd_ss4798.py
@@ -0,0 +1,19 @@
+def gcd(a: int, b: int) -> int:
+      """
+      Calculate the greatest common divisor (GCD) of two integers a and b
+      using the Euclidean algorithm.
+      """
+      # Implement your solution here
+      
+      if a ==0 and b==0:
+            return None
+      a= abs(a)
+      b= abs(b)
+      
+      if b == 0:
+            return a
+      return gcd(b, a % b)
+
+print(gcd(54, 24))  # Expected output: 6
+print(gcd(48, 18))  # Expected output: 6
+print(gcd(101, 10))  # Expected output: 1
